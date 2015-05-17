7856735c456ccaf2ff244bc84f01458bd2345501
Akash Manohar J <akash@akash.im>
2015-05-17 19:11:57 +0530

    Fix rubygems_plugin

--- rubygems-plugin/rubygems_plugin.rb
+++ rubygems-plugin/rubygems_plugin.rb
@@ -1,10 +1,11 @@
-# Yes totally inspired by rbenv. It's like I couldn't take my mind off that implementation
+# Yes borrowed from rbenv. Couldn't take my mind off that implementation
 
 Gem.post_install do |installer|
-  puts installer.spec.executables.inspect
-  puts installer.bin_dir.inspect
-  # if installer.spec.executables.any? && installer.bin_dir == Gem.default_bindir
-     # TODO read from dir name
-     # `asdf reshim ruby #{RUBY_VERSION} installer.spec.executables`
-  # end
+
+  if installer.spec.executables.any? && installer.bin_dir == Gem.default_bindir
+    installer.spec.executables.each do |executable|
+     `asdf reshim ruby #{RUBY_VERSION} bin/#{executable}`
+   end
+  end
+
 end
