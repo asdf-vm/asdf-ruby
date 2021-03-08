fa4760090af85ea8d2c9e461f38fae8361b854a3
Seivan Heidari <seivan.heidari@icloud.com>
2021-03-09 00:15:04 +0100

    * Replacing ruby-build with ruby-install. * Will use existing `ruby-install` from `$PATH` or fetch specific version when setting ASDF_RUBY_INSTALL_VERSION. * Will allow ruby-install use existing dependencies from package manager instead of building from source. * Fix: https://github.com/asdf-vm/asdf-ruby/issues/204

--- README.md
+++ README.md
@@ -10,28 +10,45 @@ Ruby plugin for [asdf](https://github.com/asdf-vm/asdf) version manager
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 ```
 
-Please make sure you have the required [system dependencies](https://github.com/rbenv/ruby-build/wiki#suggested-build-environment) installed before trying to install Ruby.
+Please make sure you have the required [system dependencies](https://github.com/postmodern/ruby-install#requirements) installed before trying to install Ruby.
+
+By default, asdf-ruby will try to rely as much as it can on your [package manager](https://github.com/postmodern/ruby-install#features) for both using ruby-install as well as fetching necessary dependencies to build from source.
 
 ## Use
 
 Check [asdf](https://github.com/asdf-vm/asdf) readme for instructions on how to install & manage versions of Ruby.
 
-When installing Ruby using `asdf install`, you can pass custom configure options with the [env vars supported by ruby-build](https://github.com/rbenv/ruby-build#custom-build-configuration).
+When installing Ruby using `asdf install`, you can pass custom configure options with the [env vars supported by ruby-install](https://github.com/postmodern/ruby-install#synopsis).
 
-Under the hood, asdf-ruby uses [ruby-build](https://github.com/rbenv/ruby-build) to build and install Ruby, check its [README](https://github.com/rbenv/ruby-build/blob/master/README.md) for more information about build options and the [troubleshooting](https://github.com/rbenv/ruby-build/wiki#troubleshooting) wiki section for any issues encountered during installation of ruby versions.
+Under the hood, asdf-ruby uses [ruby-install](https://github.com/postmodern/ruby-install) to build and install Ruby, check its [README](https://github.com/postmodern/ruby-install) for more information about build options and the [requirements](https://github.com/postmodern/ruby-install#requirements) section for any issues encountered during installation of ruby versions.
 
 You may also apply custom patches before building with `RUBY_APPLY_PATCHES`, e.g.
 
-```
+```sh
 RUBY_APPLY_PATCHES=$'dir/1.patch\n2.patch\nhttp://example.com/3.patch' asdf install ruby 2.4.1
-RUBY_APPLY_PATCHES=$(curl -s https://raw.githubusercontent.com/rvm/rvm/master/patchsets/ruby/2.1.1/railsexpress) asdf install ruby 2.1.1
+RUBY_APPLY_PATCHES="https://raw.githubusercontent.com/rvm/rvm/master/patchsets/ruby/2.1.1/railsexpress" asdf install ruby 2.1.1
 ```
 
-By default asdf-ruby uses the latest release of ruby-build, but you can choose your own branch/tag through the `ASDF_RUBY_BUILD_VERSION` variable:
+Although unecessary with ruby-install, you can still pass custom options
 
+```sh
+RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)" asdf install ruby 2.4.0
 ```
-ASDF_RUBY_BUILD_VERSION=master asdf install ruby 2.6.4
+
+```sh
+RUBY_CONFIGURE_OPTS="--enable-shared --enable-dtrace CFLAGS="-O3"" asdf install ruby 2.4.0
 ```
+
+By default asdf-ruby uses the latest release of ruby-install, but you can choose your own branch/tag through the `ASDF_RUBY_INSTALL_VERSION` variable:
+
+```sh
+ASDF_RUBY_INSTALL_VERSION="v0.8.0" asdf install ruby 2.6.4
+```
+
+```sh
+ASDF_RUBY_INSTALL_VERSION="master" asdf install ruby 2.6.4
+```
+
 
 ## Default gems
 
--- lib/utils.sh
+++ lib/utils.sh
@@ -1,65 +1,68 @@
-RUBY_BUILD_VERSION="${ASDF_RUBY_BUILD_VERSION:-v20201225}"
-RUBY_BUILD_TAG="$RUBY_BUILD_VERSION"
+RUBY_INSTALL_VERSION="${ASDF_RUBY_INSTALL_VERSION:-v0.8.1}"
 
 echoerr() {
   >&2 echo -e "\033[0;31m$1\033[0m"
 }
 
-ensure_ruby_build_setup() {
-  ensure_ruby_build_installed
+ensure_ruby_install_setup() {
+  ensure_ruby_install_installed
 }
 
-ensure_ruby_build_installed() {
-    local ruby_build_version
-
-    if [ ! -f "$(ruby_build_path)" ]; then
-        download_ruby_build
-    else
-        current_ruby_build_version="$("$(ruby_build_path)" --version | cut -d ' ' -f2)"
-        # If ruby-build version does not start with 'v',
-        # add 'v' to beginning of version
-        if [ ${current_ruby_build_version:0:1} != "v" ]; then
-          current_ruby_build_version="v$current_ruby_build_version"
-        fi
-        if [ "$current_ruby_build_version" != "$RUBY_BUILD_VERSION" ]; then
-            # If the ruby-build directory already exists and the version does not
-            # match, remove it and download the correct version
-            rm -rf "$(ruby_build_dir)"
-            download_ruby_build
-        fi
+ensure_ruby_install_installed() {
+  # If ruby-install exists
+  if [ -x "$(ruby_install_path)" ]; then
+    # But was passed an expected version
+    if [ -n "${ASDF_RUBY_INSTALL_VERSION:-}" ]; then
+      current_ruby_install_version="v$("$(ruby_install_path)" --version | cut -d ' ' -f2)"
+      # Check if expected version matches current version
+      if [ "$current_ruby_install_version" != "$RUBY_INSTALL_VERSION" ]; then
+        # If not reinstall and checkout ASDF_RUBY_INSTALL_VERSION
+        rm -rf "$(ruby_install_dir)"
+        download_ruby_install
+      fi
     fi
+  else
+    # ruby-install does not exist, so install using default value in RUBY_INSTALL_VERSION
+    download_ruby_install
+  fi
 }
 
-download_ruby_build() {
-    # Print to stderr so asdf doesn't assume this string is a list of versions
-    echoerr "Downloading ruby-build..."
-    local build_dir="$(ruby_build_source_dir)"
 
-    # Remove directory in case it still exists from last download
-    rm -rf $build_dir
 
-    # Clone down and checkout the correct ruby-build version
-    git clone https://github.com/rbenv/ruby-build.git $build_dir >&2 >/dev/null
-    (cd $build_dir; git checkout $RUBY_BUILD_TAG >&2 >/dev/null)
+download_ruby_install() {
+  # Print to stderr so asdf doesn't assume this string is a list of versions
+  echoerr "Downloading ruby-install $RUBY_INSTALL_VERSION"
+  local build_dir="$(ruby_install_source_dir)"
 
-    # Install in the ruby-build dir
-    PREFIX="$(ruby_build_dir)" $build_dir/install.sh
+  # Remove directory in case it still exists from last download
+  rm -rf "$build_dir"
 
-    # Remove ruby-build source dir
-    rm -rf $build_dir
+  # Clone down and checkout the correct ruby-install version
+  git clone https://github.com/postmodern/ruby-install.git "$build_dir" --quiet
+  (cd "$build_dir"; git checkout $RUBY_INSTALL_VERSION --quiet;)
+
+  echo "$(cd $build_dir; PREFIX=$(ruby_install_dir) make install)" 2>&1 >/dev/null
+
+  rm -rf "$build_dir"
 }
 
 asdf_ruby_plugin_path() {
-    echo "$(dirname "$(dirname "$0")")"
+  echo "$(dirname "$(dirname "$0")")"
 }
-ruby_build_dir() {
-    echo "$(asdf_ruby_plugin_path)/ruby-build"
-}
-
-ruby_build_source_dir() {
-    echo "$(asdf_ruby_plugin_path)/ruby-build-source"
+ruby_install_dir() {
+  echo "$(asdf_ruby_plugin_path)/ruby-install"
 }
 
-ruby_build_path() {
-    echo "$(ruby_build_dir)/bin/ruby-build"
+ruby_install_source_dir() {
+  echo "$(asdf_ruby_plugin_path)/ruby-install-source"
+}
+
+
+ruby_install_path() {
+  #Check if ruby-install exists without an expected version
+  if [ -x "$(command -v ruby-install)" ] && [ -z "${ASDF_RUBY_INSTALL_VERSION:-}" ]; then
+    echo "$(command -v ruby-install)"
+  else
+    echo "$(ruby_install_dir)/bin/ruby-install"
+  fi
 }
