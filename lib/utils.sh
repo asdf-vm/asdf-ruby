56f76acaf58398cbdce0a5a4cffaa46b80765972
Seivan Heidari <seivan.heidari@icloud.com>
2021-03-31 09:28:44 +0200

    Add prebuilt binaries for macOS. Opt out via ASDF_RUBY_FROM_SOURCE

--- lib/utils.sh
+++ lib/utils.sh
@@ -71,3 +71,111 @@ ruby_install_executable() {
     echo "$(ruby_install_path)/bin/ruby-install"
   fi
 }
+
+get_macos_marketing_name() {
+  if [ "$(uname -s)" = "Darwin" ]; then
+    osx_num=$(SYSTEM_VERSION_COMPAT=1 sw_vers -productVersion | awk -F '[.]' '{print $2}')
+    OSX_MARKETING=(
+        ["10"]="yosemite"
+        ["11"]="el_capitan"
+        ["12"]="sierra"
+        ["13"]="high_sierra"
+        ["14"]="mojave"
+        ["15"]="catalina"
+        ["16"]="big_sur"
+      )
+
+    if [[ -n "${OSX_MARKETING[$osx_num]}" ]]; then 
+      printf '%s\n' "${OSX_MARKETING[$osx_num]}"
+      return 0
+    else
+      return 1
+    fi
+  else
+    return 1
+  fi
+}
+
+macos_get_download_url() {
+  local os_name=$(
+      if [ "$(uname -m)" = "arm64" ]; then 
+        printf "arm64_%s" "$1"
+      else
+        printf "%s" "$1"
+      fi
+    )
+
+  printf 'Looking for prebuilt %s %s on %s. \n' "$(plugin_name)" "$ASDF_INSTALL_VERSION" "$os_name" >&2
+  local ruby_version="ruby-$ASDF_INSTALL_VERSION"
+  local file_name="$ruby_version.$os_name.bottle.tar.gz"
+  local url="https://bintray.com/homebrew/bottles/download_file?file_path=$file_name"
+
+  if curl --output /dev/null --silent --head --fail "$url"; then
+    printf '%s' $url
+    return 0
+  else
+    printerr 'No prebuilt %s %s for %s is available. ' "$(plugin_name)" "$ASDF_INSTALL_VERSION" "$os_name"
+    return 1
+  fi
+}
+
+macos_update_linked_paths() {
+  
+  local file_paths=("$@")
+  
+  local brew_prefix="$(brew --prefix)"
+  local replacement_cellar="@@HOMEBREW_CELLAR@@/ruby/$ASDF_INSTALL_VERSION"
+
+  local replacement_prefix="@@HOMEBREW_PREFIX@@"
+  
+
+  for file in "${file_paths[@]}"; do
+
+    local changes=()
+    for line in $(otool -L "$file"); do
+      if [[ "$line" == "$replacement_cellar"* ]]; then
+        local result="${line/$replacement_cellar/$ASDF_INSTALL_PATH}"
+        changes+=("-change" "$line" "$result")
+      fi
+      if [[ "$line" == "$replacement_prefix"* ]]; then
+        local result="${line/$replacement_prefix/$brew_prefix}"
+        changes+=("-change" "$line" "$result")
+      fi
+    done
+    local command=(install_name_tool "${changes[@]}" "$file")
+    "${command[@]}"
+
+    # https://github.com/Homebrew/brew/blob/565becc90433df57c9ec6262dec1f41797fb680b/Library/Homebrew/os/mac/keg.rb#L21
+    cp "$file" "$file.backup"
+    codesign -s - -f -vvvvvv "$file.backup"
+    mv -f "$file.backup" "$file"
+  done
+
+
+
+
+}
+
+macos_check_homebrew_setup() {
+  
+  local brew_function="${1:-PREBUILT}"
+  
+  printf 'Checking Homebrew for dependencies. \n'
+  if [ -x "$(command -v brew)" ]; then
+    if [ ! -d "$(brew --prefix openssl)" ]; then
+      printerr 'Missing openssl from Homebrew'
+      printf 'Fix with: brew install openssl \n'
+      exit 1
+    fi
+    if [ ! -d "$(brew --prefix readline)" ]; then
+      printerr 'Missing wxmac from Homebrew'
+      printf 'Fix with: brew install readline \n'
+      exit 1
+    fi
+    return 0
+  else
+      printerr 'Homebrew is not installed or in PATH'
+      printf 'To install without Homebrew, set your own KERL_CONFIGURE_OPTIONS \n'
+      exit 1
+  fi
+}
